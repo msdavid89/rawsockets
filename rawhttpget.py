1d0a6e27a896ec6315e5ab9b6c39b61224bb0987
Michael Davidson <michaeldavidson@ccs.neu.edu>
2017-03-11 16:30:08 -0500

    Basic argument parsing.

---
+++ rawhttpget.py
@@ -0,0 +1,12 @@
+import argparse
+import urllib
+
+
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Project 4: Raw Sockets')
+    parser.add_argument('url', help='URL to use for raw socket communication.', required=True)
+    args = parser.parse_args()
