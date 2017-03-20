104cad103d7c86ff6844907a64d465849e08474f
Michael Davidson <michaeldavidson@ccs.neu.edu>
2017-03-20 15:27:12 -0400

    Slightly more things.

--- rawhttpget.py
+++ rawhttpget.py
@@ -1,6 +1,9 @@
 import argparse
+import sys
 import urllib
 import socket
+import struct
+import random
 
 
 class IPHeader:
@@ -21,8 +24,20 @@ class IPHeader:
     |                    Options                    |    Padding    |
     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"""
 
-    def __init__(self):
-        self.version = 0b0100
+    def __init__(self, data):
+        self.version = 4
+        self.ihl = 5
+        self.tos = 0
+        self.length = 20
+        self.id = 0
+        self.flags = 0
+        self.offset = 0
+        self.ttl = 0
+        self.proto = socket.IPPROTO_TCP
+        self.chksum = 0
+        self.src = 0
+        self.dst = 0
+        self.data = data
 
 
 
@@ -51,8 +66,31 @@ class TCPHeader:
 
 
 
+class RawGet:
+
+    def __init__(self, url):
+        self.url = url
+        self.sendsock = -1
+        self.recvsock = -1
+
+
+    def start(self):
+        try:
+            self.sendsock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
+            self.recvsock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+        except socket.error:
+            print("Failed to create socket. Womp womp.")
+            sys.exit()
+
+
+
+
 
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.exit("Usage: ./rawhttpget <URL>")
     parser = argparse.ArgumentParser(description='Project 4: Raw Sockets')
     parser.add_argument('url', help='URL to use for raw socket communication.', required=True)
     args = parser.parse_args()
+    rawget = RawGet(args.url)
+    rawget.start()
